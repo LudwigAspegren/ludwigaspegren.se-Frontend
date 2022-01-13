012fa50d51778db0c445fe8b685d9a0a9d692c83
ludwigaspegren <1450952+LudwigAspegren@users.noreply.github.com>
2022-01-13 12:04:08 +0100

    moved api url to env variables

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="@sveltejs/kit" />
+
+interface ImportMetaEnv {
+    VITE_API_PATH: string
+}
+
+export const variables = {
+    basePath: import.meta.env.VITE_API_PATH
+}
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
--- src/stores/flickrStore.ts
+++ src/stores/flickrStore.ts
@@ -1,16 +1,16 @@
+import { variables } from "$lib/variables";
 import type PhotoViewModel from "$types/photoViewModel";
 import { emptyPhotosetViewModel, emptyPhotoViewModel } from "$utils/utils";
 import type PhotosetViewModel from "src/types/photosetViewModel";
 import type { Writable } from "svelte/store";
 import { writable } from "svelte/store";
-
 export const photo: Writable<PhotoViewModel> = writable(emptyPhotoViewModel());
 export const photoset: Writable<PhotosetViewModel> = writable(emptyPhotosetViewModel());
 export const photosets: Writable<PhotosetViewModel[]> = writable([]);
 
 
 const fetchPhotoset = async (id: string): Promise<PhotosetViewModel> => {
-    const url = `https://ludwigaspegren-api.azurewebsites.net/api/GetPhotoset/${id}`;
+    const url = `${variables.apiPath}/GetPhotoset/${id}`;
     const res = await fetch(url);
     const data: PhotosetViewModel = await res.json();
     return data;
