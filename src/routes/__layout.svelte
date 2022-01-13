78ac6ec65640b8fb1c692fc21ed6a18bf081c318
ludwigaspegren <1450952+LudwigAspegren@users.noreply.github.com>
2022-01-13 12:01:48 +0100

    removed unused load function parameter

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,10 @@
 <script context="module" lang="ts">
-	import Footer from '$components/footer.svelte';
-	import Navigation from '$components/navigation.svelte';
-	import { photosets } from '$stores/flickrStore';
-	import '../app.css';
-	export async function load({ page, session }: any) {
-		photosets.set(session.response);
-		return {};
-	}
+  import { photosets } from '$stores/flickrStore'
+  import '../app.css'
+  export async function load({ session }: any) {
+    photosets.set(session.response)
+    return {}
+  }
 </script>
 
 <slot />
