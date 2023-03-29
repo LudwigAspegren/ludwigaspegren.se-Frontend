ed14f8d58495f4a26ce0beb993a6f59498a1680d
ludwigaspegren <1450952+LudwigAspegren@users.noreply.github.com>
2023-03-29 18:15:54 +0200

    empty commit

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,7 @@
 import { fetchAll, photosetIds } from '$stores/flickrStore'
 import '../app.css'
-import type { LayoutLoad } from './$types'
 
-export const load: LayoutLoad = async () => {
+export async function load() {
   const photosets = await fetchAll(photosetIds)
   const photos = photosets.map(photoSet => { return photoSet.photos }).flat()
   return {
